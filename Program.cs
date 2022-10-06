8be5d2bca51376af3f4b047ecbb86fd0e4ad7a1f
Julia Kutyrkina <u_kutyrkina@mail.ru>
2022-10-06 22:56:21 +0300

    Рабочая

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// №10, вторая цифра трехзначного числа
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = (n/10)%10;
+Console.WriteLine(result);
+
